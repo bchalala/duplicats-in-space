9fb695d252dcae1822fbab955a1c6718a2a525e3
Brett Chalabian <brettchalabian@gmail.com>
2016-11-21 20:39:12 -0800

    Changed dict values to be seq rather than set. This fixed a bug where string was deconstructed into characters and characters were inserted into the set.

--- parsePapers.py
+++ parsePapers.py
@@ -8,19 +8,18 @@ def matchingPaperIds():
     duplicates = set()
 
     with open("dataRev2/Paper.csv") as csvfile:
-        fn = ['id', 'title', 'year', 'conferenceid', 'journalid', 'keywords']
-        reader = csv.DictReader(csvfile, fieldnames=fn)
+        reader = csv.DictReader(csvfile)
         for row in reader:
-            title = row['title']
-            paperId = (row['id'], row['conferenceid'], row['journalid'])
+            title = row['Title']
+            paperId = row['Id']
 
             if title is "":
                 continue   
             if title in paperDict:
-                paperDict[title].add(paperId)
+                paperDict[title].append(paperId)
                 duplicates.add(title)
             else:
-                paperDict[title] = set(paperId)
+                paperDict[title] = [paperId]
 
     for entry in duplicates:
         print(entry)
